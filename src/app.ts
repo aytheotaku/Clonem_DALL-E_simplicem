3003f2991f5d90950b102c2766f28972c278be9b
Ayomide <ayakinbo@gmail.com>
2023-08-10 05:01:41 +0100

    cleaning up

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,44 @@
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
+import createError from 'http-errors';
 import { getHomePage } from './controllers/getHomePage.js';
+import { generateImageController } from './controllers/generateImageController.js';
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express();
 const PORT  = process.env.PORT
 
+
+
 app.use(express.json())
-app.get('/', getHomePage);
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', getHomePage)
+app.post('/generateImage', generateImageController);
+
+
+
+
+app.use(async (req:Request, res:Response, next:NextFunction) => {
+    next(createError.NotFound())
+})
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+app.use((err:any, req:Request, res:Response, next: NextFunction) => {
+    res.status(err.status || 500)
+    res.send({
+        success: "false",
+        message: "An error has occurred",
+        status: err.status || 500,
+        error: [{
+            message: err.message
+        }]
+    })
+})
+
